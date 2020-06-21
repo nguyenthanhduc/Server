7b360548851003517ecfa96b834b31a0adb57258
nguyenthanhduc4444 <nguyenthanhduc4444@gmail.com>
2020-06-21 19:05:03 +0700

    fix resend

--- controller/product.js
+++ controller/product.js
@@ -37,16 +37,23 @@ exports.createproduct = async (req,res) => {
 
 exports.updateproduct = async (req, res) => {
     try {
-        const updateProduct = await Product.updateOne(
-            { id: req.body.id },
-            {
-                $set: {
-                    name: req.body.name,
-                    price: req.body.price
+        console.log(req.body.id)
+        const product= await Product.find();
+        console.log(product[product.length-1].id);
+        if (req.body.id > product[product.length-1].id) 
+            res.status(404).json({error: "Product id does not exists"})
+        else{
+            const updateProduct = await Product.updateOne(
+                { id: req.body.id },
+                {
+                    $set: {
+                        name: req.body.name,
+                        price: req.body.price
+                    }
                 }
-            }
-        );
-        res.json(updateProduct);
+            );
+            res.json(updateProduct);
+        }
     } catch (err) {
         res.json({ message: err })
     }
@@ -54,6 +61,11 @@ exports.updateproduct = async (req, res) => {
 
 exports.deleteproduct = async (req, res) => {
     try {
+        console.log(req.params.deleteid);
+        const product= await Product.find();
+        console.log(product[product.length-1].id);
+        if (req.params.deleteid > product[product.length-1].id) 
+            res.status(404).json({error: "Product id does not exists"})
         await Product.deleteOne({ id: req.params.deleteid });
         res.json({ message: 'Product deleted' });
     } catch (err) {
